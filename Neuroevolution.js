aed92fddbab2cd9aeb1cbbe28588760f3737c269
Romain Gille <romain.gille@gmail.com>
2018-04-13 16:14:31 +0200

    rm class Layer, use of array instead

--- Neuroevolution.js
+++ Neuroevolution.js
@@ -90,35 +90,12 @@ var Neuroevolution = function (options) {
         }
     }
 
-
-    /*LAYER***********************************************************************/
-    /**
-     * Neural Network Layer class.
-     *
-     * @constructor
-     * @param {index} Index of this Layer in the Network.
-     */
-    var Layer = function (index) {
-        this.id = index || 0;
-        this.neurons = [];
-    }
-
-    /**
-     * Populate the Layer with a set of randomly weighted Neurons.
-     *
-     * Each Neuron be initialied with nbInputs inputs with a random clamped
-     * value.
-     *
-     * @param {nbNeurons} Number of neurons.
-     * @param {nbInputs} Number of inputs.
-     * @return void
-     */
-    Layer.prototype.populate = function (nbNeurons, nbInputs) {
-        this.neurons = [];
+    populate = function (layer, nbNeurons, nbInputs) {
+        neurons = [];
         for (var i = 0; i < nbNeurons; i++) {
             var n = new Neuron();
             n.populate(nbInputs);
-            this.neurons.push(n);
+            layer.push(n);
         }
     }
 
@@ -146,25 +123,26 @@ var Neuroevolution = function (options) {
     Network.prototype.perceptronGeneration = function (input, hiddens, output) {
         var index = 0;
         var previousNeurons = 0;
-        var layer = new Layer(index);
-        layer.populate(input, previousNeurons); // Number of Inputs will be set to
+        var layer = [];//new Layer(index);
+        populate(layer, input, previousNeurons); // Number of Inputs will be set to
         // 0 since it is an input layer.
         previousNeurons = input; // number of input is size of previous layer.
         this.layers.push(layer);
         index++;
         for (var i in hiddens) {
             // Repeat same process as first layer for each hidden layer.
-            var layer = new Layer(index);
-            layer.populate(hiddens[i], previousNeurons);
+            var layer = [];//new Layer(index);
+            populate(layer, hiddens[i], previousNeurons);
             previousNeurons = hiddens[i];
             this.layers.push(layer);
             index++;
         }
-        var layer = new Layer(index);
-        layer.populate(output, previousNeurons); // Number of input is equal to
+        var layer = [];//new Layer(index);
+        populate(layer, output, previousNeurons); // Number of input is equal to
         // the size of the last hidden
         // layer.
         this.layers.push(layer);
+        console.log(this.layers);
     }
 
     /**
@@ -181,12 +159,12 @@ var Neuroevolution = function (options) {
         };
 
         for (var i in this.layers) {
-            datas.neurons.push(this.layers[i].neurons.length);
-            for (var j in this.layers[i].neurons) {
-                for (var k in this.layers[i].neurons[j].weights) {
+            datas.neurons.push(this.layers[i].length);
+            for (var j in this.layers[i]) {
+                for (var k in this.layers[i][j].weights) {
                     // push all input weights of each Neuron of each Layer into a flat
                     // array.
-                    datas.weights.push(this.layers[i].neurons[j].weights[k]);
+                    datas.weights.push(this.layers[i][j].weights[k]);
                 }
             }
         }
@@ -206,12 +184,12 @@ var Neuroevolution = function (options) {
         this.layers = [];
         for (var i in save.neurons) {
             // Create and populate layers.
-            var layer = new Layer(index);
-            layer.populate(save.neurons[i], previousNeurons);
-            for (var j in layer.neurons) {
-                for (var k in layer.neurons[j].weights) {
+            var layer = [];//new Layer(index);
+            populate(layer, save.neurons[i], previousNeurons);
+            for (var j in layer) {
+                for (var k in layer[j].weights) {
                     // Apply neurons weights to each Neuron.
-                    layer.neurons[j].weights[k] = save.weights[indexWeights];
+                    layer[j].weights[k] = save.weights[indexWeights];
 
                     indexWeights++; // Increment index of flat array.
                 }
@@ -231,25 +209,25 @@ var Neuroevolution = function (options) {
     Network.prototype.compute = function (inputs) {
         // Set the value of each Neuron in the input layer.
         for (var i in inputs) {
-            if (this.layers[0] && this.layers[0].neurons[i]) {
-                this.layers[0].neurons[i].value = inputs[i];
+            if (this.layers[0] && this.layers[0][i]) {
+                this.layers[0][i].value = inputs[i];
             }
         }
 
         var prevLayer = this.layers[0]; // Previous layer is input layer.
         for (var i = 1; i < this.layers.length; i++) {
-            for (var j in this.layers[i].neurons) {
+            for (var j in this.layers[i]) {
                 // For each Neuron in each layer.
                 var sum = 0;
-                for (var k in prevLayer.neurons) {
+                for (var k in prevLayer) {
                     // Every Neuron in the previous layer is an input to each Neuron in
                     // the next layer.
-                    sum += prevLayer.neurons[k].value *
-                        this.layers[i].neurons[j].weights[k];
+                    sum += prevLayer[k].value *
+                        this.layers[i][j].weights[k];
                 }
 
                 // Compute the activation of the Neuron.
-                this.layers[i].neurons[j].value = self.options.activation(sum);
+                this.layers[i][j].value = self.options.activation(sum);
             }
             prevLayer = this.layers[i];
         }
@@ -257,8 +235,8 @@ var Neuroevolution = function (options) {
         // All outputs of the Network.
         var out = [];
         var lastLayer = this.layers[this.layers.length - 1];
-        for (var i in lastLayer.neurons) {
-            out.push(lastLayer.neurons[i].value);
+        for (var i in lastLayer) {
+            out.push(lastLayer[i].value);
         }
         return out;
     }
