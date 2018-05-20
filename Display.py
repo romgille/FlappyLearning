3fe81b1113dc74a3b83a16e6c5e12c9bf9ff2f76
Aire-One <erwan.maugere@gmail.com>
2018-05-20 14:58:23 +0200

    Prepare refactor

--- Display.py
+++ Display.py
@@ -1,4 +1,5 @@
 #!/bin/python
+
 import tkinter
 import os, inspect
 import time
@@ -8,12 +9,15 @@ from PIL import ImageFont
 from PIL import ImageTk
 from PIL import ImageDraw
 
-import Bird
-import Pipe
+# not sure it's needed to import Drawable to use Drawable.update() ?
+import Drawable
 
 class Display:
+
+    # Move that fucking const garbage to a global file/class for configs ?
     WIDTH = 500
     HEIGHT = 512
+    WINDOW_TITLE = "Flappy Learning"
     BLACK_COLOR = "#000000"
     WHITE_COLOR = "#FFFFFF"
     FONT = ImageFont.truetype(font="fonts/Roboto-Regular.ttf", size=30)
@@ -21,42 +25,49 @@ class Display:
     YYY = 200
 
     def __init__(self):
-        # Window
+        # Open a Window
         self.window = tkinter.Tk()
-        self.window.title("Flappy Learning")
-        # Image
+        self.window.title(WINDOW_TITLE)
+
+        # Prepare the Image Buffer
         self.buffer = Image.new("RGBA", (WIDTH, HEIGHT))
 
-        # Background
-        backgroundImage = Image.open("img/background.png").convert("RGBA")
-        xBg, yBg = myBackgroundImage.size
-        self.background = Image.new("RGBA", (WIDTH, HEIGHT))
-        self.background.paste(backgroundImage, (xBg, 0, xBg*2, yBg), backgroundImage)
-        self.background.paste(backgroundImage, (0, 0, xBg, yBg), backgroundImage)
+        # Background (need to be moved to a new class)
+        # backgroundImage = Image.open("img/background.png").convert("RGBA")
+        # xBg, yBg = myBackgroundImage.size
+        # self.background = Image.new("RGBA", (WIDTH, HEIGHT))
+        # self.background.paste(backgroundImage, (xBg, 0, xBg*2, yBg), backgroundImage)
+        # self.background.paste(backgroundImage, (0, 0, xBg, yBg), backgroundImage)
 
-    def draw(birds, pipes):
-        # Draw the background
+        # Prepare the Drawing Object
         draw = ImageDraw.Draw(buffer)
-        # Canvas to draw
+
+        # Prepare the Canvas
         canvas = tkinter.Canvas(self.window, width=WIDTH-1, height=HEIGHT-1, bg=BLACK_COLOR)
         img = tkinter.PhotoImage(width=WIDTH, height=HEIGHT)
         ECRAN = canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
         canvas.pack()
 
+    def close(self):
+        window.destroy()
+
+    def draw(self, drawables):
+        # wipe the current image
         draw.rectangle(((0,0), (WIDTH,HEIGHT)), fill=myBlackColor)
-        buffer.paste(myBackground, (-yyy,0))
-        buffer.paste(myBackground, (-yyy+WIDTH,0))
 
-        # Draw birds
-        for bird in birds:
-            bird.draw(buffer)
-        # Draw pipes
-        for pipe in pipes:
-            pipe.draw(buffer)
+        # background stuff, need to be moved to a new class
+        # buffer.paste(myBackground, (-yyy,0))
+        # buffer.paste(myBackground, (-yyy+WIDTH,0))
 
-        # Draw text delta
-        draw.text((30, 5),"delta: " + str(delta), font=FONT, fill=WHITE_COLOR)
-        yyy = (yyy+4) % WIDTH
+        # Draw
+        for d in drawables:
+            d.draw(buffer)
+
+        # Draw text delta (need clock stuff)
+        # draw.text((30, 5),"delta: " + str(delta), font=FONT, fill=WHITE_COLOR)
+
+        # Background related stuff need to be managed by background
+        # yyy = (yyy+4) % WIDTH
 
         # Transfert de la zone de dessin vers l'ecran
         photo = PIL.ImageTk.PhotoImage(buffer)
